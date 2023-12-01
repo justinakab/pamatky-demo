9503ffb63e70b2b703e07dc5eed906b445ccb68d
EmSturm <emsturmova@gmail.com>
2023-12-01 13:08:35 +0100

    Components DM, Footer, Header, Menu + HomePage + description v data.js

---
+++ src/components/DesktopMenu/index.jsx
@@ -0,0 +1,20 @@
+import './style.css';
+import { Link } from 'react-router-dom';
+
+export const DesktopMenu = () => {
+  return (
+    <div className="menuDesktop">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/monuments">památky</Link>
+          </li>
+          <li>
+            <Link to="/game">hry & testy</Link>
+          </li>
+          <li><Link to="/AboutProject">O projektu</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
--- src/pages/MonumentListPage/index.jsx
+++ src/pages/MonumentListPage/index.jsx
@@ -1,14 +1,27 @@
-import { Link } from 'react-router-dom';
-import { monuments } from '../../../lib/data';
+import "./style.css";
+import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
+import { Link } from "react-router-dom";
+import { monuments } from "../../../lib/data";
 
 export const MonumentListPage = () => {
   return (
     <>
-      {monuments.map((monument) => (
-        <li key={monument.id}>
-          <Link to={`/monument-list/${monument.id}`}>{monument.name}</Link>
-        </li>
-      ))}
+      <Header></Header>
+      <div className="list">
+        <div className="list__left--col">
+          <ul className="monuments-list"></ul>
+          {monuments.map((monument) => (
+            <li key={monument.id}>
+              <Link to={`/monument-list/${monument.id}`}>{monument.name}</Link>
+            </li>
+          ))}
+        </div>
+        <div className="list__right--col">
+          <div className="orloj--img"></div>
+        </div>
+      </div>
+      <Footer></Footer>
     </>
   );
 };
