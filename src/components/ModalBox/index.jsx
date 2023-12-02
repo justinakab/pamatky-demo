675e05e3e8918623daad2db9ff44ce9d1ee1f469
Justina Kabelková <justina.kabel@gmail.com>
2023-12-02 14:08:51 +0100

    úprava ModalBox

--- src/components/ModalBox/index.jsx
+++ src/components/ModalBox/index.jsx
@@ -32,24 +32,25 @@ export const ModalBox = ({ onIsModalOpen }) => {
           </svg>
         </div>
         <div className="modal-box-about--container">
-          <p className="modal-box--text">
-            Cíl:{' '}
-            <span className="highlight">
-              Umísti všechny pražské památky správně do mapy.
+          <div className="modal-box--text">
+            <span className="highlight highlight--bigger">
+              Cíl: Umísti všechny pražské památky správně do mapy.
             </span>{' '}
             <br></br> <br />
-            Jak na to? <br />
-            <ol>
-              <li>Vpravo na stránce najdeš celou řadu pražských památek.</li>
-              <li>
-                Postupně zkus každou památku přetáhnou na jedno ze žlutých míst
-                na mapě.
-              </li>
-              <li>Pokud ji umístíš správně, památka ti v mapě zůstane.</li>
-            </ol>
-            Nakonec si každou památku můžeš rozkliknout a přečíst si o ní
-            zajímavé informace. Užij si to!
-          </p>
+            <span className="highlight--bigger">Jak na to?</span>
+            <br />
+            <p>
+              Vpravo na stránce najdeš celou řadu pražských památek. <br></br>{' '}
+              <br></br>
+              Postupně zkus každou památku přetáhnou na jedno ze žlutých míst na
+              mapě. <br></br> <br></br>
+              Pokud ji umístíš správně, památka ti v mapě zůstane.
+              <br></br> <br />
+              Nakonec si každou památku můžeš rozkliknout a přečíst si o ní
+              zajímavé informace. <br></br> <br />
+              <span className="highlight--bigger">Užij si to!</span>
+            </p>
+          </div>
           <div className="modal-box-gif"></div>
         </div>
       </div>
